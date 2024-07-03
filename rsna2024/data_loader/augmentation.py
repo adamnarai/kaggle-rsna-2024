fix data normalization and add split model

rsna2024/data_loader/augmentation.py
@@ -10,22 +10,15 @@ class TransformFactory():
         return self.aug
 
 class NoAug(TransformFactory):
-    def __init__(self, layer_num):
+    def __init__(self):
         self.aug = v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
 
-class Normalize(TransformFactory):
-    def __init__(self, layer_num):
-        self.aug = v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True),
-                               v2.Normalize(mean=[0.5]*layer_num, std=[0.5]*layer_num)])
-
 class Elastic(TransformFactory):
-    def __init__(self, layer_num):
+    def __init__(self):
         self.aug = v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True),
-                               v2.ElasticTransform(), 
-                               v2.Normalize(mean=[0.5]*layer_num, std=[0.5]*layer_num)])
+                               v2.ElasticTransform()])
 
 class Affine(TransformFactory):
-    def __init__(self, layer_num, p=0.5):
+    def __init__(self, p=0.5):
         self.aug = v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True),
-                               v2.RandomAffine(degrees=10, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=10),
-                               v2.Normalize(mean=[0.5]*layer_num, std=[0.5]*layer_num)])
+                               v2.RandomAffine(degrees=10, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=10)])

rsna2024/data_loader/data_loaders.py
@@ -1,6 +1,6 @@
 from torch.utils.data import DataLoader
 
-from .datasets import RSNA2024Dataset
+from .datasets import RSNA2024Dataset, RSNA2024SplitDataset
 
 
 class RSNADataLoader(DataLoader):
@@ -8,6 +8,23 @@ class RSNADataLoader(DataLoader):
         self, df, transform, phase, data_dir, out_vars, img_num, batch_size, shuffle, num_workers, pin_memory
     ):
         dataset = RSNA2024Dataset(df, data_dir, out_vars, img_num=img_num, transform=transform)
+        if phase == 'train':
+            pass
+        elif phase == 'valid':
+            shuffle = False
+        super().__init__(
+            dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            num_workers=num_workers,
+            pin_memory=pin_memory,
+        )
+
+class RSNASplitDataLoader(DataLoader):
+    def __init__(
+        self, df, transform, phase, data_dir, out_vars, img_num, batch_size, shuffle, num_workers, pin_memory
+    ):
+        dataset = RSNA2024SplitDataset(df, data_dir, out_vars, img_num=img_num, transform=transform)
         if phase == 'train':
             pass
         elif phase == 'valid':

rsna2024/data_loader/datasets.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import pydicom
 import cv2
-cv2.setNumThreads(0)
 
 import torch
 import torch.nn.functional as F
@@ -12,7 +11,7 @@ from torch.utils.data import Dataset
 from rsna2024.utils import natural_sort
 
 class RSNA2024Dataset(Dataset):
-    def __init__(self, df, data_dir, out_vars, img_num=(10, 10, 10), transform=None):
+    def __init__(self, df, data_dir, out_vars, img_num, transform=None):
         self.df = df
         self.df_series = self.load_series_info(data_dir)
         self.img_dir = os.path.join(data_dir, 'train_images')
@@ -25,21 +24,17 @@ class RSNA2024Dataset(Dataset):
 
     def __getitem__(self, idx):
         row = self.df.iloc[idx]
-        x = np.zeros((512, 512, sum(self.img_num)), dtype=np.uint8)
         label = self.df[self.out_vars].iloc[idx].values
         label = np.nan_to_num(label.astype(float), nan=0).astype(np.int64)
 
         # Sagittal T1
-        x = self.add_series(x, row.study_id, 'Sagittal T1', img_num=self.img_num[0], offset=0)
-        x = self.add_series(x, row.study_id, 'Sagittal T2/STIR', img_num=self.img_num[1], offset=self.img_num[0])
-        x = self.add_series(x, row.study_id, 'Axial T2', img_num=self.img_num[2], offset=sum(self.img_num[:2]))
+        x1 = self.get_series(row.study_id, 'Sagittal T1', img_num=self.img_num[0])
+        x2 = self.get_series(row.study_id, 'Sagittal T2/STIR', img_num=self.img_num[1])
+        x3 = self.get_series(row.study_id, 'Axial T2', img_num=self.img_num[2])
+        x = np.concatenate([x1, x2, x3], axis=2)
         
         if self.transform:
             x = self.transform(x)
-        
-        # # Albumentations
-        # if self.transform:
-        #     x = self.transform(image=x)['image']
 
         return x, label
     
@@ -54,7 +49,8 @@ class RSNA2024Dataset(Dataset):
             
         return series_list.values[0]
   
-    def add_series(self, x, study_id, series_description, img_num=10, offset=0, resolution=(512, 512)):
+    def get_series(self, study_id, series_description, img_num, resolution=(512, 512)):
+        x = np.zeros((*resolution, img_num), dtype=np.float32)
         series_id = self.get_series_id(study_id, series_description)
         if series_id is None:
             return x
@@ -62,16 +58,39 @@ class RSNA2024Dataset(Dataset):
         series_dir = os.path.join(self.img_dir, study_id, series_id)
         file_list = natural_sort(os.listdir(series_dir))
 
-        if len(file_list) >= img_num:
+        if len(file_list) > img_num:
             start_index = (len(file_list)-img_num) // 2
             file_list = file_list[start_index:start_index + img_num]
 
         for i, filename in enumerate(file_list):
             ds = pydicom.dcmread(os.path.join(series_dir, filename))
             img = ds.pixel_array.astype(np.float32)
-            img = cv2.normalize(img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
             img = cv2.resize(img, resolution, interpolation=cv2.INTER_CUBIC)
 
-            x[..., i + offset] = img
+            x[..., i] = img
+
+        # Standardize series
+        x = (x - x.mean()) / x.std()
 
         return x
+    
+class RSNA2024SplitDataset(RSNA2024Dataset):
+    def __init__(self, df, data_dir, out_vars, img_num, transform=None):
+        super().__init__(df, data_dir, out_vars, img_num, transform)
+
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        label = self.df[self.out_vars].iloc[idx].values
+        label = np.nan_to_num(label.astype(float), nan=0).astype(np.int64)
+
+        # Sagittal T1
+        x1 = self.get_series(row.study_id, 'Sagittal T1', img_num=self.img_num[0])
+        x2 = self.get_series(row.study_id, 'Sagittal T2/STIR', img_num=self.img_num[1])
+        x3 = self.get_series(row.study_id, 'Axial T2', img_num=self.img_num[2])
+        
+        if self.transform:
+            x1 = self.transform(x1)
+            x2 = self.transform(x2)
+            x3 = self.transform(x3)
+
+        return x1, x2, x3, label