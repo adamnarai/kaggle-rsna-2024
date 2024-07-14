94022d15bebaa0cb4ec50c1ef0f99a5b03d8f446
Ádám Nárai <narai.adam@gmail.com>
2024-07-14 19:30:18 +0200

    fix num_workers>0 bug

--- rsna2024/main.py
+++ rsna2024/main.py
@@ -1,5 +1,13 @@
+import os
 import cv2
 cv2.setNumThreads(0)
+cv2.ocl.setUseOpenCL(False)
+
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
 
 from rsna2024.runner import Runner
 from rsna2024.utils import load_config
